7032eda4061929b862935cb1a0806ce36e646fc6
nikolinaneric <nericnikolina@gmail.com>
2023-04-07 20:18:54 +0200

    added few API endopoints

--- website/tasks.py
+++ website/tasks.py
@@ -3,7 +3,6 @@ from flask_mail import Message
 from flask import current_app
 from . import mail
 
-
 @shared_task(bind = True)
 def send_mail(self, subject, recipients, message_body, reply_to = 'dogs.people.connect@gmail.com'):
     msg = Message(subject = subject, sender = current_app.config['MAIL_DEFAULT_SENDER'], recipients= recipients, reply_to=reply_to)
